330c0d984d90d38becda067c58dfcd6cd5b1f0d9
OGU <OGU@3d187648-d01e-7443-9418-ef00e4b4781c>
2015-09-16 10:11:13 +0000

    Code Reordering

--- YammerTestConsole/TestRewardPuan/Default.aspx.cs
+++ YammerTestConsole/TestRewardPuan/Default.aspx.cs
@@ -9,12 +9,5 @@ namespace TestRewardPuan
         {
 
         }
-
-        protected void btnSignIn_Click(object sender, EventArgs e)
-        {
-            var url = string.Format("https://www.yammer.com/oauth2/authorize?client_id={0}&response_type=code&redirect_uri={1}",
-                Constants.ClientID, Constants.RedirectUrl);
-            Response.Redirect(url);
-        }
     }
 }
--- YammerTestConsole/TestRewardPuan/PointsTable.aspx.cs
+++ YammerTestConsole/TestRewardPuan/PointsTable.aspx.cs
@@ -1,11 +1,7 @@
 ﻿using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace TestRewardPuan
 {
